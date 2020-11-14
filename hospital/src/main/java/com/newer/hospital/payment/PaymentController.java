09c0d059dc88c2707d8e97a0a67874140880cf06
13886711507sjk <www.2645968117@qq.com>
2020-11-14 10:55:40 +0800

    修改注解

--- hospital/src/main/java/com/newer/hospital/payment/PaymentController.java
+++ hospital/src/main/java/com/newer/hospital/payment/PaymentController.java
@@ -6,18 +6,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.newer.hospital.communal.entity.Paymenthistory;
 import com.newer.hospital.communal.entity.Prescription;
 import com.newer.hospital.communal.util.mapper.PaymenthisoryMapper;
 import com.newer.hospital.communal.util.mapper.PresciptionMapper;
 
+@CrossOrigin
 @EnableAutoConfiguration(exclude = { SecurityAutoConfiguration.class })
 @RestController
 @RequestMapping("/api/payment")
